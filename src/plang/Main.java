f6b3fba2436ae666424141fbce95f348e83f41e1
JuraSciix <jurasciix@gmail.com>
2026-03-26 09:02:34 +0300

    Комментирует код, выводящий структуру АСД

--- src/plang/Main.java
+++ src/plang/Main.java
@@ -20,9 +20,9 @@ public class Main {
         LexerResult lexerResult = lexer.tokenize("test", str);
         ParserResult parserResult = parser.parse(lexerResult);
 
-        AstPrintVisitor printVisitor = new AstPrintVisitor();
-        parserResult.getStatements().forEach(stmt -> stmt.accept(printVisitor));
-        printVisitor.flush();
+//        AstPrintVisitor printVisitor = new AstPrintVisitor();
+//        parserResult.getStatements().forEach(stmt -> stmt.accept(printVisitor));
+//        printVisitor.flush();
 
         Code code = new Code();
         Gen gen = new Gen(code);
