9a6195c8e1ba22a998c98027b72e804a7d674bad
JuraSciix <jurasciix@gmail.com>
2026-03-26 07:12:17 +0300

    Добавляет исчерпывающую информацию об инструкциях в `OPCodeInfo`

--- src/plang/interpreter/OPCodeInfo.java
+++ src/plang/interpreter/OPCodeInfo.java
@@ -1,65 +1,103 @@
 package plang.interpreter;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static plang.interpreter.OPCodeList.*;
 
 public class OPCodeInfo {
-
-    private static final Map<Integer, String> M = new HashMap<>();
+    private static final OPCodeInfo[] INFO = new OPCodeInfo[0xff];
 
     static {
-        M.put(nop, "nop");
+        INFO[nop] = info("nop");
 
-        M.put(add, "add");
-        M.put(sub, "sub");
-        M.put(mul, "mul");
-        M.put(div, "div");
-        M.put(rem, "rem");
-        M.put(bit_and, "bit-and");
-        M.put(bit_or, "bit-or");
-        M.put(bit_xor, "bit-xor");
-        M.put(bit_inv, "bit-inv");
+        // Бинарные и унарные операции.
+        INFO[add] = info("add", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[sub] = info("sub", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[mul] = info("mul", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[div] = info("div", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[rem] = info("rem", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[bit_and] = info("bit-and", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[bit_or] = info("bit-or", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[bit_xor] = info("bit-xor", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[bit_inv] = info("bit-inv", Param.INDEX, Param.INDEX, Param.INDEX);
+        INFO[neg] = info("neg", Param.INDEX, Param.INDEX);
 
-        M.put(neg, "neg");
+        // Загружает значение из пула констант.
+        INFO[load] = info("load", Param.INDEX, Param.INDEX);
 
-        M.put(load, "load");
-        M.put(reset, "reset");
-        M.put(mov, "mov");
+        // Зануляет регистр/
+        INFO[reset] = info("reset", Param.INDEX);
 
-        M.put(cmp_eq, "cmp-eq");
-        M.put(cmp_ne, "cmp-ne");
+        // Копирует значение из левого регистра в правый.
+        INFO[mov] = info("mov", Param.INDEX, Param.INDEX);
 
-        M.put(jump, "jump");
-        M.put(jmp_eq, "jmp-eq");
-        M.put(jmp_ne, "jmp-ne");
+        // Сравнивают два значения и переключают флаг.
+        INFO[cmp_eq] = info("cmp-eq", Param.INDEX, Param.INDEX);
+        INFO[cmp_ne] = info("cmp-ne", Param.INDEX, Param.INDEX);
+        INFO[cmp_lt] = info("cmp-lt", Param.INDEX, Param.INDEX);
+        INFO[cmp_ge] = info("cmp-ge", Param.INDEX, Param.INDEX);
+        INFO[cmp_gt] = info("cmp-gt", Param.INDEX, Param.INDEX);
+        INFO[cmp_le] = info("cmp-le", Param.INDEX, Param.INDEX);
 
-        M.put(_return, "ret");
-        M.put(leave, "leave");
+        // Безусловный прыжок.
+        INFO[jump] = info("jump", Param.DOUBLE_INDEX);
+
+        // Условный прыжок, если флаг активен.
+        INFO[jmp_z] = info("jmp-z", Param.DOUBLE_INDEX);
+
+        // Условный прыжок, если флаг неактивен.
+        INFO[jmp_nz] = info("jmp-nz", Param.DOUBLE_INDEX);
+
+        INFO[_return] = info("ret", Param.INDEX);
+        INFO[leave] = info("leave");
     }
 
+    private static OPCodeInfo info(String name, Param... params) {
+        return new OPCodeInfo(name, params);
+    }
+
+    @Deprecated
     public static String opcodeString(int opcode) {
-        // OPxAA
-        return M.getOrDefault(opcode, String.format("OPx%02x", opcode));
+        return info(opcode).name;
     }
 
+    @Deprecated
     public static int weight(int opcode) {
-        if ((add <= opcode && opcode <= bit_xor)
-                || (cmp_eq <= opcode && opcode <= cmp_ne)
-                || (jmp_eq <= opcode && opcode <= jmp_ne)) {
-            return 4;
+        return info(opcode).getWeight();
+    }
+
+    public static OPCodeInfo info(int opcode) {
+        if (0 > opcode || opcode >= 0xff || INFO[opcode] == null) {
+            throw new IllegalArgumentException("Illegal opcode: " + opcode);
+        }
+        return INFO[opcode];
+    }
+
+    public final String name;
+    public final Param[] params;
+
+    private OPCodeInfo(String name, Param[] params) {
+        this.name = name;
+        this.params = params;
+    }
+
+    public int getWeight() {
+        int w = 1; // Opcode byte
+
+        for (Param param : params) {
+            switch (param) {
+                case INDEX:
+                    w += 1;
+                    break;
+                case DOUBLE_INDEX:
+                    w += 2;
+                    break;
+            }
         }
 
-        if (opcode == neg || opcode == bit_inv
-                || opcode == mov || opcode == load) {
-            return 3;
-        }
+        return w;
+    }
 
-        if (opcode == jump || opcode == _return || opcode == reset) {
-            return 2;
-        }
-
-        return 1;
+    public enum Param {
+        INDEX, // 1 byte
+        DOUBLE_INDEX // 2 bytes
     }
 }
