d88a14b9022ccd9a14e3903ed16197cb166c964f
JuraSciix <jurasciix@gmail.com>
2026-09-05 08:20:52 +0300

    Добавляет метод `IOUtils.readCharBufferFromPath()`

--- src/plang/utils/IOUtils.java
+++ src/plang/utils/IOUtils.java
@@ -4,6 +4,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class IOUtils {
 
@@ -17,5 +22,12 @@ public class IOUtils {
             }
         }
         return output.toString();
+    }
+
+    public static CharBuffer readCharBufferFromPath(Path path, Charset charset) throws IOException {
+        try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
+            long size = channel.size();
+            return charset.decode(channel.map(FileChannel.MapMode.READ_ONLY, 0, size));
+        }
     }
 }
