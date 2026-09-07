ae79a87a7e2ebd5128e3c8885276b5cc5c599ee3
JuraSciix <jurasciix@gmail.com>
2026-09-07 12:54:59 +0300

    Рефакторит `Bytes`

--- src/plang/interpreter/Bytes.java
+++ src/plang/interpreter/Bytes.java
@@ -1,12 +1,15 @@
 package plang.interpreter;
 
-class Bytes {
+public final class Bytes {
+    private Bytes() {
+        throw new AssertionError();
+    }
 
-    static int readUB(byte[] arr, int off) {
+    public static int readUB(byte[] arr, int off) {
         return arr[off] & 0xff;
     }
 
-    static int read2UB(byte[] arr, int off) {
+    public static int read2UB(byte[] arr, int off) {
         return readUB(arr, off) | readUB(arr, off + 1) << 8;
     }
 }
