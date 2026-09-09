fe0063c48aa8bd6faf9ff7efad2794540c940128
JuraSciix <jurasciix@gmail.com>
2026-09-09 09:09:06 +0300

    Добавляет замеры перед выполнением

--- src/plang/Main.java
+++ src/plang/Main.java
@@ -9,52 +9,130 @@ import plang.utils.IOUtils;
 import java.io.IOException;
 import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+    private static final boolean PRINT_AST = false;
+    private static final boolean PRINT_ASM = false;
+
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("input", "foo.pl");
-        CharBuffer content = IOUtils.readCharBufferFromPath(path, StandardCharsets.UTF_8);
+        String name = "foo.pl";
+        CharBuffer content = IOUtils.readCharBufferFromPath(
+                Paths.get("input", name),
+                StandardCharsets.UTF_8);
+        measureLexer(name, content);
 
         Lexer lexer = new Lexer();
+        LexResult lexResult = lexer.tokenize(name, content);
+        measureParser(lexResult);
+
         Parser parser = new Parser();
-
-        LexResult lexResult = lexer.tokenize("test", content);
         ParseResult parseResult = parser.parse(lexResult);
+        if (PRINT_AST) {
+            AstPrintVisitor printVisitor = new AstPrintVisitor();
+            parseResult.getAst().accept(printVisitor);
+            printVisitor.flush();
+        }
+        measureGen(parseResult);
 
-//        AstPrintVisitor printVisitor = new AstPrintVisitor();
-//        parserResult.getStatements().forEach(stmt -> stmt.accept(printVisitor));
-//        printVisitor.flush();
+        CodeData data = translate(parseResult);
+        if (PRINT_ASM) {
+            CodePrinter codePrinter = new CodePrinter();
+            codePrinter.print(data.code, data.constantPool);
+        }
+        measureRun(data);
 
+        long result = run(data);
+        System.out.println("Result: " + result);
+    }
+
+    private static CodeData translate(ParseResult parseResult) {
+        // Все аллокации после выхода из метода должны освободиться
         Code code = new Code();
         CodeEmitter emitter = new CodeEmitter();
         Gen gen = new Gen(code, emitter);
-        long ctx = System.nanoTime();
         Items.Item resultItem = gen.gen(parseResult.getAst());
         if (resultItem.alive()) {
             // Добавляем return
             gen.gen(new Ast.Return(0, null));
         }
-        long cty = System.nanoTime();
-        long ctm = (cty - ctx) / 1000;
+        return gen.getData();
+    }
 
-        CodeData data = gen.getData();
-        CodePrinter codePrinter = new CodePrinter();
-        codePrinter.print(data.code, data.constantPool);
-        System.out.println("Compiled in " + ctm + " hs. Instructions: " + data.code.length);
+    private static long run(CodeData data) {
+        long[] memoryData = new long[256];
+        BytecodeInterpreter interpreter = new BytecodeInterpreter();
+        int result = interpreter.run(data.code, data.constantPool, 0, memoryData, 0, 10);
+        return memoryData[result];
+    }
+
+    private static void measureLexer(String name, CharBuffer content) {
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
+        Lexer lexer = new Lexer();
+        for (int i = 0; i < 10; i++) {
+            long tx = System.nanoTime();
+            for (int j = 0; j < 10; j++) {
+                lexer.tokenize(name, content);
+            }
+            long ty = System.nanoTime();
+            long dt = (ty - tx) / 10;
+            max = Math.max(max, dt);
+            min = Math.min(min, dt);
+        }
+        System.out.println("Lexing measurement: " + max + " - " + min + " mcs");
+    }
+
+    private static void measureParser(LexResult lexResult) {
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
+        Parser parser = new Parser();
+        for (int i = 0; i < 10; i++) {
+            long tx = System.nanoTime();
+            for (int j = 0; j < 10; j++) {
+                parser.parse(lexResult);
+            }
+            long ty = System.nanoTime();
+            long dt = (ty - tx) / 10;
+            max = Math.max(max, dt);
+            min = Math.min(min, dt);
+        }
+        System.out.println("Parsing measurement: " + max + " - " + min + " mcs");
+    }
+
+    private static void measureGen(ParseResult parseResult) {
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            Code code = new Code();
+            CodeEmitter emitter = new CodeEmitter();
+            Gen gen = new Gen(code, emitter);
+            long tx = System.nanoTime();
+            for (int j = 0; j < 5; j++) {
+                parseResult.getAst().accept(gen);
+            }
+            long ty = System.nanoTime();
+            long dt = (ty - tx) / (5 * 1000);
+            max = Math.max(max, dt);
+            min = Math.min(min, dt);
+        }
+        System.out.println("Generating measurement: " + max + " - " + min + " mcs");
+    }
+
+    private static void measureRun(CodeData data) {
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
 
         long[] memoryData = new long[256];
         BytecodeInterpreter interpreter = new BytecodeInterpreter();
-
-        int n = 1;
-        for (int i = 0; i < n; i++) {
-            long etx = System.nanoTime();
-            int result = interpreter.run(data.code, data.constantPool, 0, memoryData, 0, 10);
-            long ety = System.nanoTime();
-
-            long etm = (ety - etx) / 1000;
-            System.out.println("Executed in " + etm + " hs. Result: " + memoryData[result]);
+        for (int i = 0; i < 10; i++) {
+            long tx = System.nanoTime();
+            interpreter.run(data.code, data.constantPool, 0, memoryData, 0, 10);
+            long ty = System.nanoTime();
+            long dt = (ty - tx) / 1000;
+            max = Math.max(max, dt);
+            min = Math.min(min, dt);
         }
+        System.out.println("Executed measurement: " + max + " - " + min + " mcs");
     }
 }
